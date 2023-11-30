119eb0f83cfbb4ba770ec788c2c1d74d65696d50
Bryan Schneller <bryan.schneller@gmail.com>
2023-11-30 15:53:53 -0500

    Add 10 more items to each array, and end each sentence with a period

--- script.js
+++ script.js
@@ -14,15 +14,22 @@ let button = document.getElementById('button1');
 //make 4 arrays of words: adjectives, nouns, -ing verbs, adverbs
 const adj = ['happy', 'sad', 'funny', 'stupid', 'beautiful',
 'boastful', 'tired', 'self-absorbed', 'scary', 'timid', 'stressed-out', 'evil',
-'ugly', 'thirsty', 'loathsome', 'old', 'striped', 'green', 'sickly', 'bored'];
+'ugly', 'thirsty', 'loathsome', 'old', 'striped', 'green', 'sickly', 'bored',
+'grouchy', 'enormous', 'British', 'foolhardy', 'smooth-talking', 'catlike', 
+'mustachioed', 'Dickensian', 'overblown', 'flea-ridden'];
 const noun = ['tree', 'nurse', 'bat', 'cat', 'bulldozer', 'bowl of oatmeal', 'dentist',
 'librarian', 'car', 'football', 'television', 'car battery', 'bonsai tree', 'lawyer', 'alien life-form', 
-'random message generator', 'painting', 'computer', 'nerd', 'soundcloud rapper'];
+'random message generator', 'painting', 'computer', 'nerd', 'soundcloud rapper',
+'dock worker', 'Prime Minister', 'pinecone', 'squirrel', 'end table', 'author',
+'cashier', 'cutting board', 'eagle', 'charcuterie board'];
 const verb = ['lifting', 'licking', 'bothering', 'yelling at', 'scraping', 'slapping', 'removing', 
 'chewing on', 'polishing', 'cleaning', 'asking to dinner', 'defeating', 'paying', 'feeding', 
-'running away from', 'apologizing to', 'partnering up with', 'writing to', 'glaring at', 'laughing at'];
+'running away from', 'apologizing to', 'partnering up with', 'writing to', 'glaring at', 'laughing at',
+'sizing up', 'seizing', 'investigating', 'mailing a letter to', 'pestering', 'idolizing', 
+'shaking hands with', 'approaching', 'questioning', 'befriending'];
 const adv = ['quickly', 'sadly', 'badly', 'sorrowfully', 'happily', 'slowly', 'angrily', 'whinily', 'cordially', 
-'secretly', 'eagerly', 'carefully', 'quietly', 'worriedly', 'grimly', 'deeply', 'tomorrow', 'soon', 'too often', 'sloppily'];
+'secretly', 'eagerly', 'carefully', 'quietly', 'worriedly', 'grimly', 'deeply', 'tomorrow', 'soon', 'too often', 'sloppily',
+'without care', 'carelessly', 'like a moron', 'neatly', 'sweetly', 'at Town Hall', 'in Germany', 'quite poorly', 'desperately', 'unsurprisingly'];
 
 //make 6 random numbers labeled a through f 
 
@@ -36,12 +43,12 @@ let f;
 //function that assigns the inner HTML of appropriate boxes to values at random indexes of the arrays
 
 const newMessage = () => {
-    a = Math.floor(Math.random()*20);
-    b = Math.floor(Math.random()*20);
-    c = Math.floor(Math.random()*20);
-    d = Math.floor(Math.random()*20);
-    e = Math.floor(Math.random()*20);
-    f = Math.floor(Math.random()*20);
+    a = Math.floor(Math.random()*30);
+    b = Math.floor(Math.random()*30);
+    c = Math.floor(Math.random()*30);
+    d = Math.floor(Math.random()*30);
+    e = Math.floor(Math.random()*30);
+    f = Math.floor(Math.random()*30);
 
     box1.style.display = 'flex';
     box2.style.display = 'flex';
@@ -61,7 +68,7 @@ const newMessage = () => {
     box6.innerHTML = "the";
     box7.innerHTML = adj[d];
     box8.innerHTML = noun[e];
-    box9.innerHTML = adv[f];
+    box9.innerHTML = adv[f] + ".";
 }
 
 button.onclick = newMessage;
